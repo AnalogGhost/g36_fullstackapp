c5cf9141b138ef8d4ffff23e2d36b6e0db1acf65
Mathew Brown <matt@mattbrown.com>
2016-12-01 13:24:58 -0700

    Add post route to users route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var route = express.Router();
 
+
 route.get('/',(req,res,next) => {
   res.send('Users');
+});
+
+route.post('/', (req,res,next) => {
+  res.send(req.body);
 });
 
 module.exports = route;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
