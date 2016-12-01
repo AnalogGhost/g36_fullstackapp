90ea04c810b32e62e71ba71e6b3ade8409d0eb38
Mathew Brown <matt@mattbrown.com>
2016-12-01 13:59:46 -0700

    Update \users post route - user registration inserted into database

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,36 @@ var express = require('express');
 var route = express.Router();
 var bcrypt = require('bcrypt');
 
+var db = require('../knexfile.js')['development'];
+
+var knex = require('knex')(db);
+
 route.get('/',(req,res,next) => {
   res.send('Users');
 });
 
 route.post('/', (req,res,next) => {
   var hash = bcrypt.hashSync(req.body.password, 8);
-  res.send({username: req.body.username, hash: hash});
+
+  knex('users')
+  .where({username: req.body.username})
+  .then(function (results) {
+    if (results.length === 0) {
+      knex('users')
+      .insert({
+          username: req.body.username,
+          password_hash: hash
+      })
+      .then(function (result) {
+        res.send("User Created");
+      })
+      .catch(function (err) {
+        next(err);
+      });
+    } else {
+      res.status(400).send('User Already Exists');
+    }
+  });
 });
 
 module.exports = route;
