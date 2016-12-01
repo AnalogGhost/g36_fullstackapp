5124937df2d6278cfabfca2ed2c9c41062bc7f91
Mathew Brown <matt@mattbrown.com>
2016-12-01 15:11:21 -0700

    Create login route and handle invalid logins

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var route = express.Router();
 
+var bcrypt = require('bcrypt');
+
+var db = require('../knexfile.js')['development'];
+var knex = require('knex')(db);
+
 route.get('/',(req,res,next) => {
   res.send('Root');
+});
+
+route.post('/login', (req,res,next) => {
+  if (!req.body.username || !req.body.password) {
+    res.sendStatus(400);
+  }
+
+  knex('users')
+  .where({username: req.body.username})
+  .first()
+  .then(function (result) {
+    if (!result || !bcrypt.compareSync(req.body.password,result.password_hash)) {
+      res.sendStatus(401);
+    } else {
+      
+    }
+  })
+
 });
 
 module.exports = route;
