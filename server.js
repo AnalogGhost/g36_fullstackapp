f7653297409a66f9ad311c34440ff3d4770b96a7
Mathew Brown <matt@mattbrown.com>
2016-12-01 13:17:30 -0700

    Add routes folder and index route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var index = require('./routes/index');
+
+app.use('/',index);
+
 app.listen('3000', function () {
   console.log("Listening on port 3000");
 });
